5b062262aa7ee6f2a3113e96c68734c9f65e517d
vvuksan-fastly <67602984+vvuksan-fastly@users.noreply.github.com>
2023-07-27 12:30:29 -0400

    Add CORS headers VCL to TF

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ locals {
   vcl_remove_cookies_headers  = file("${path.module}/vcl/remove_cookies_headers.vcl")
   vcl_remove_response_headers = file("${path.module}/vcl/remove_response_headers.vcl")
   vcl_segmented_caching       = file("${path.module}/vcl/segmented_caching.vcl")
+  vcl_cors_headers            = file("${path.module}/vcl/add_cors_headers.vcl")
 }
 
 resource "fastly_service_vcl" "files_service" {
@@ -112,6 +113,13 @@ resource "fastly_service_vcl" "files_service" {
     name     = "Remove headers from origin response"
     content  = local.vcl_remove_response_headers
     type     = "fetch"
+    priority = 100
+  }
+
+  snippet {
+    name     = "Add CORS headers if necessary"
+    content  = local.vcl_cors_headers
+    type     = "deliver"
     priority = 100
   }
 
